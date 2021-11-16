ee2e5b8c95284f0c538ce44874ff49a69926f21e
Seth <Medeirosseth@gamil.com>
2021-11-16 11:40:31 -0800

    display multple choices

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ let gameData;
 const main = document.querySelector('main')
 const pokemonImage = document.querySelector('#pokemon-image')
 const playBtn = document.querySelector('#play')
+const choice = document.querySelector('#choices')
 
 
 playBtn.addEventListener('click', fetchData);
@@ -10,8 +11,18 @@ async function fetchData() {
   gameData = await window.getPokeData();
   console.log(gameData)
   showSilhouette();
+  displayChoices();
 }
 
 function showSilhouette(){
   pokemonImage.src = gameData.correct.image;
+}
+
+function displayChoices() {
+  const { pokemonChoices } = gameData;
+  const choicesHTML = pokemonChoices.map((({ name}) => {
+    return `<button data-name="${name}">${name}</button>`
+  })).join('')
+
+  choices.innerHTML = choicesHTML;
 }
