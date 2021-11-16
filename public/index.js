34536f34d3e2f5ba230870aabaad46f4b26a438c
Seth <Medeirosseth@gamil.com>
2021-11-16 11:34:27 -0800

    add a display of pokemon silhouette

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 let gameData;
 const main = document.querySelector('main')
+const pokemonImage = document.querySelector('#pokemon-image')
 const playBtn = document.querySelector('#play')
 
 
@@ -8,4 +9,9 @@ playBtn.addEventListener('click', fetchData);
 async function fetchData() {
   gameData = await window.getPokeData();
   console.log(gameData)
+  showSilhouette();
+}
+
+function showSilhouette(){
+  pokemonImage.src = gameData.correct.image;
 }
