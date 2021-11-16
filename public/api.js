e8e41b94b82137b01b257d72a73a7188f0787bb2
Seth <Medeirosseth@gamil.com>
2021-11-16 10:47:31 -0800

    add shuffled pokemon function

--- public/api.js
+++ public/api.js
@@ -1,5 +1,7 @@
 window.getPokeData = async function() {
   const pokemon = await getPokemon();
+  const randomPokemon = shuffle(pokemon);
+  console.log(randomPokemon)
 };
 
 
@@ -7,4 +9,16 @@ async function getPokemon() {
   const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
   const pokemon = await res.json()
   console.log(pokemon)
+
+  return pokemon.results
+}
+
+function shuffle(unshuffled) {
+  const shuffled = unshuffled
+    .map(value => ({value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(( {value }) => value);
+
+    return shuffled
+
 }
